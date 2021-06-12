db84cf4724f28c3e574dc6696ec194e996e3d9f4
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2021-06-12 23:36:53 +0300

    hata-yonetimi

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi :" + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("İşlem tammalandı."); 
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-2000000000000");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+           
+        }
+    }
+}
