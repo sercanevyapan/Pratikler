e339112b80fb1c23ae11b6245c0ad5bb1bacf8af
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2021-06-15 00:07:14 +0300

    diziler

---
+++ diziler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            Console.Write("Lütfen dizinin eleman sayisini giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayıDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısı giriniz", i + 1);
+                sayıDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayıDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama :" + toplam / diziUzunlugu);
+
+        }
+    }
+}
