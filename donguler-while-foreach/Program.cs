255821a68557dda5270bd1d49f53a2d84d951bfe
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2021-06-14 20:57:16 +0300

    donguler-while-foreach

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 den başlayarak console dan girilen sayıya kadar ortalama hesapla
+            Console.Write("Bİr sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            //'a' dan 'z' ye kadar tüm harfleri yazdır
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            Console.Write("Foreach");
+            string[] arabalar = { "BMV", "FOrd", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
