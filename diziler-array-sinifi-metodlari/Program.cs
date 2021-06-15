c77c87d99ca5464b456ac3a9debc8552a1dbe87b
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2021-06-15 14:53:30 +0300

    diziler-array-sinifi-metodlari

---
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {23, 12, 4, 86, 72, 3, 11, 17};
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            Array.Clear(sayiDizisi,2,2);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Array.IndexOf(sayiDizisi, 23);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+        }
+   }
+}
