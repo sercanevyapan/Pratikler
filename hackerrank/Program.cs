6365bec4c9d3c14f8e8346686871666663e36ea3
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2021-06-15 21:08:17 +0300

    metotlar

---
+++ hackerrank/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            //for (var i = n - 1; i > -1; i--)
+            //{
+            //    Console.Write(arr[i] + " ");
+
+            //}
+
+            /*
+             * Day 20: Sorting
+
+
+             */
+
+            //    int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            //// Write your code here
+            //int numberOfSwaps = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    // Track number of elements swapped during a single array traversal
+
+
+
+
+
+            //    for (int j = 0; j < n - 1; j++)
+            //    {
+
+            //        // Swap adjacent elements if they are in decreasing order
+            //        if (a[j] > a[j + 1])
+            //        {
+            //            int temp = a[j];
+            //            a[j] = a[j + 1];
+            //            a[j + 1] = temp;
+
+            //            numberOfSwaps++;
+
+
+            //        }
+            //    }
+
+            //    // If no elements were swapped during a traversal, array is sorted
+            //    if (numberOfSwaps == 0)
+            //    {
+            //        break;
+            //    }
+
+            //}
+            //Console.WriteLine("Array is sorted in " + numberOfSwaps + " swaps.");
+            //Console.WriteLine("First Element: " + a[0]);
+            //Console.WriteLine("Last Element: " + a[n - 1]);
+
+
+            /*
+             *Simple Array Sum
+             */
+
+            //int sum = 0;
+            //foreach (int num in ar)
+            //{
+            //    sum = num + sum;
+            //}
+            //return sum;
+
+            /*
+             *Compare the Triplets
+             */
+
+            //List<int> score = new List<int>(new int[] { 0, 0 });
+            //int i = 0;
+
+            //for (i = 0; i < 3; i++)
+            //{
+            //    if (a[i] > b[i])
+            //    {
+            //        score[0] += 1;
+            //    }
+            //    else if (a[i] < b[i])
+            //    {
+            //        score[1] += 1;
+            //    }
+
+            //}
+            //return score;
+
+
+
+
+        }
+    }
+}
---
+++ metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+           // Console.WriteLine(a+b);
+
+           int sonuc = Topla(a,b);
+           //Console.WriteLine(sonuc);
+
+           Metotlar ornek = new Metotlar();
+           ornek.EkranaYazdır(sonuc.ToString());
+
+           int sonuc2 = ornek.ArttırTopla(ref a, ref b);
+            ornek.EkranaYazdır(Convert.ToString(sonuc2));
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdır(String veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırTopla(ref int deger1, ref int deger2)
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1 + deger2;
+        }
+
+    }
+}
