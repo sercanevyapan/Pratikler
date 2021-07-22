4581805f98a70db805e146af647ffd2bc5155c74
Sercan Evyapan <sercanevyapan@outlook.com>
2021-07-22 19:34:21 +0300

    generic_list

---
+++ generic_list/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<T> class
+            //System.collection.Generic
+            // T-> object türündedir
+
+            List<int> sayiListesi = new List<int>();
+
+            sayiListesi.Add(23);
+            sayiListesi.Add(10);
+            sayiListesi.Add(4);
+            sayiListesi.Add(5);
+            sayiListesi.Add(92);
+            sayiListesi.Add(34);
+
+            List<string> renkListesi = new List<string>();
+            renkListesi.Add("Kırmızı");
+            renkListesi.Add("Mavi");
+            renkListesi.Add("Turuncu");
+            renkListesi.Add("Sarı");
+            renkListesi.Add("Yeşil");
+
+            //count
+            Console.WriteLine(renkListesi.Count);
+            Console.WriteLine(sayiListesi.Count);
+
+            //Forech ve List.Foreach ile elemanlara erişim
+            foreach (var sayi in sayiListesi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            foreach (var sayi in renkListesi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            //Listeden eleman çıkarma
+            sayiListesi.Remove(4);
+            renkListesi.Remove("Yeşil");
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            sayiListesi.RemoveAt(0);
+            renkListesi.RemoveAt(1);
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            //Liste içerisinde arama
+            if (sayiListesi.Contains(10))
+                Console.WriteLine("10 liste içerisinde bulundu");
+
+            //Eleman ile index'e erişimi
+            Console.WriteLine( renkListesi.BinarySearch("Turuncu"));
+
+            //Dizi List'e çevirme
+            string[] hayvanlar = { "kedi", "köpek", "kuş" };
+
+            List<string> hayvanlarListesi = new List<string>(hayvanlar);
+
+            //Listeyi nasıl temzileriz?
+            hayvanlarListesi.Clear();
+
+            //List içerisinde nesne tutmak
+            List<Kullanicilar> kullaniciListesi = new List<Kullanicilar>();
+
+            Kullanicilar kulllanici1 = new Kullanicilar();
+            kulllanici1.Isim = "Ayşe";
+            kulllanici1.Soyisim = "Yılmaz";
+            kulllanici1.Yas = 26;
+
+            Kullanicilar kulllanici2 = new Kullanicilar();
+            kulllanici1.Isim = "Özcan";
+            kulllanici1.Soyisim = "Çalışkan";
+            kulllanici1.Yas = 26;
+
+            kullaniciListesi.Add(kulllanici1);
+            kullaniciListesi.Add(kulllanici2);
+
+            List<Kullanicilar> yeniListe = new List<Kullanicilar>();
+            yeniListe.Add(new Kullanicilar() { 
+                Isim = "Deniz",
+                Soyisim = "Arda",
+                Yas=24
+            });
+
+            foreach (var kullanıcı in kullaniciListesi)
+            {
+                Console.WriteLine("Kullanıcı Adı:" + kullanıcı.Isim);
+                Console.WriteLine("Kullanıcı Soyadı:" + kullanıcı.Soyisim);
+                Console.WriteLine("Kullanıcı Yaş:" + kullanıcı.Yas);
+            }
+
+
+
+        }
+    }
+
+    public class Kullanicilar
+    {
+        private string isim;
+        private string soyisim;
+        private int yas;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int Yas { get => yas; set => yas = value; }
+    }
+}
--- hazir_metotlar_datetime_math/Program.cs
+++ hazir_metotlar_datetime_math/Program.cs
@@ -14,47 +14,47 @@ namespace hazir_metotlar_datetime_math
             Console.WriteLine(DateTime.Now.Hour);
             Console.WriteLine(DateTime.Now.Minute);
             Console.WriteLine(DateTime.Now.Second);
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.DayOfWeek);
             Console.WriteLine(DateTime.Now.DayOfYear);
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.ToLongDateString());
             Console.WriteLine(DateTime.Now.ToShortDateString());
             Console.WriteLine(DateTime.Now.ToLongTimeString());
             Console.WriteLine(DateTime.Now.ToShortTimeString());
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.AddDays(2));
             Console.WriteLine(DateTime.Now.AddHours(2));
             Console.WriteLine(DateTime.Now.AddSeconds(30));
             Console.WriteLine(DateTime.Now.AddMonths(5));
             Console.WriteLine(DateTime.Now.AddYears(10));
             Console.WriteLine(DateTime.Now.AddMilliseconds(50));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.ToString("dd"));
             Console.WriteLine(DateTime.Now.ToString("ddd"));
             Console.WriteLine(DateTime.Now.ToString("dddd"));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.ToString("MM"));
             Console.WriteLine(DateTime.Now.ToString("MMM"));
             Console.WriteLine(DateTime.Now.ToString("MMMM"));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(DateTime.Now.ToString("yy"));
             Console.WriteLine(DateTime.Now.ToString("yyyy"));
-
+            Console.WriteLine("-----------------");
             //Math
             Console.WriteLine(Math.Abs(-25));
             Console.WriteLine(Math.Sin(10));
             Console.WriteLine(Math.Cos(10));
             Console.WriteLine(Math.Tan(10));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(Math.Ceiling(22.3));
             Console.WriteLine(Math.Round(22.3));
             Console.WriteLine(Math.Round(22.7));
             Console.WriteLine(Math.Floor(22.7));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(Math.Max(2,6));
             Console.WriteLine(Math.Min(2,6));
-
+            Console.WriteLine("-----------------");
             Console.WriteLine(Math.Pow(3, 4));
             Console.WriteLine(Math.Sqrt(9));
             Console.WriteLine(Math.Log(9));
