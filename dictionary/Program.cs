ff55a7501d4efb23f821d139821862d0bdadd89b
Sercan Evyapan <sevyapan@gmail.com>
2021-07-31 22:40:40 +0300

    dictionary

---
+++ dictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+
+            kullanıcılar.Add(10, "Ayşe Yılmaz");
+            kullanıcılar.Add(12, "Ahmet Yılmaz");
+            kullanıcılar.Add(18, "Deniz Arda");
+            kullanıcılar.Add(20, "Özcan Coşar");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("****Elemanlara erişim****");
+            Console.WriteLine(kullanıcılar[12]);
+            foreach (var item in kullanıcılar)
+                Console.WriteLine(item);
+
+
+            //count
+            Console.WriteLine("****Count****");
+            Console.WriteLine(kullanıcılar.Count);
+
+            //Contains
+            Console.WriteLine("****Contains****");
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("Zikriye Ürkmez"));
+
+            //Remove
+            Console.WriteLine("****Remove****");
+            kullanıcılar.Remove(12);
+            foreach (var item in kullanıcılar)
+                Console.WriteLine(item.Value);
+
+            //Keys
+            Console.WriteLine("****Remove****");
+            foreach (var item in kullanıcılar.Keys)
+                Console.WriteLine(item);
+            //Values
+            Console.WriteLine("****Remove****");
+            foreach (var item in kullanıcılar.Values)
+                Console.WriteLine(item);
+        }
+    }
+}
