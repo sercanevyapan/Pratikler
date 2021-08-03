5ce219644fe7bedc8cd00bc59b310817d8c7b876
Sercan Evyapan <sevyapan@gmail.com>
2021-08-03 16:34:39 +0300

    sinif_kavrami2

--- sinif_kavrami/Program.cs
+++ sinif_kavrami/Program.cs
@@ -6,13 +6,8 @@ namespace sinif_kavrami
     {
         static void Main(string[] args)
         {
-
-            Calisan calisan1 = new Calisan();
-            calisan1.Ad = "Ayşe";
-            calisan1.Soyad = "Kara";
-            calisan1.No = 234568;
-            calisan1.Departman = "İnsan Kaynakları";
-
+            
+            Calisan calisan1 = new Calisan("Ayşe","Kara", 234568, "İnsan Kaynakları");
             calisan1.CalisanBilgileri();
 
             Calisan calisan2 = new Calisan();
@@ -20,8 +15,10 @@ namespace sinif_kavrami
             calisan2.Soyad = "Arda";
             calisan2.No = 1236548;
             calisan2.Departman = "Satın Alma";
-
             calisan2.CalisanBilgileri();
+
+            Calisan calisan3 = new Calisan("Zikriye", "Ürkmez");
+            calisan3.CalisanBilgileri();
         }
     }
 
@@ -32,6 +29,27 @@ namespace sinif_kavrami
         public string Soyad;
         public int No;
         public string Departman;
+
+        public Calisan(string ad, string soyad,int no, string departman )
+        {
+
+            Ad = ad;
+            Soyad = soyad;
+            No = no;
+            Departman = departman;
+        }
+        public Calisan(string ad, string soyad)
+        {
+
+            Ad = ad;
+            Soyad = soyad;
+    
+        }
+
+        public Calisan()
+        {
+
+        }
         public void CalisanBilgileri()
         {
             Console.WriteLine("Çalışan Adı:{0}",Ad);
